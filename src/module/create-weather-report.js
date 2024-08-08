8e1a37c9d25140a954679cda3f019c0d1dcb84e0
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-08-08 16:38:46 -0400

    Fix create weather report function
    Previously, this function was trying to update and create a
    weather report. To align with CRUD principles, this function
    has been split apart, so that each module only does one
    thing.

--- src/module/create-weather-report.js
+++ src/module/create-weather-report.js
@@ -1,23 +1,4 @@
-//Code included to practice fetch requests with promises syntax
-// let getWeatherReport = (city) => {
-//   fetch(
-//     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=WNL4XSRW6RREQXLUGP6FJDDWE&contentType=json`,
-//     { mode: "cors" }
-//   )
-//     .then((response) => {
-//       return response.json();
-//     })
-//     .then((response) => {
-//       return console.log(response);
-//     });
-// };
-
-async function createWeatherReport(city) {
-  const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=WNL4XSRW6RREQXLUGP6FJDDWE&contentType=json`,
-    { mode: "cors" }
-  );
-  const weatherData = await response.json();
+function weatherReportConstructor (weatherData){
   const weatherReport = {
     title: weatherData.resolvedAddress,
     currentFeelsLike: weatherData.currentConditions.feelslike,
@@ -46,7 +27,7 @@ async function createWeatherReport(city) {
     daySevenMinimumTemperature: weatherData.days[6].tempmin,
     daySevenConditionsIcon: weatherData.days[6].icon,
   };
-  console.log(weatherReport);
+  return {...weatherReport};
 }
 
-export { createWeatherReport };
+export { weatherReportConstructor };
---
+++ src/module/update-weather-report.js
@@ -0,0 +1,22 @@
+//Code included to practice fetch requests with promises syntax
+// let getWeatherReport = (city) => {
+//   fetch(
+//     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=WNL4XSRW6RREQXLUGP6FJDDWE&contentType=json`,
+//     { mode: "cors" }
+//   )
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((response) => {
+//       return console.log(response);
+//     });
+// };
+
+async function createWeatherReport(city) {
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=WNL4XSRW6RREQXLUGP6FJDDWE&contentType=json`,
+    { mode: "cors" }
+  );
+  const weatherData = await response.json();
+}
+
