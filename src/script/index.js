ba66f7d520e8be988849ecf0d3992cc75735b273
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-08-08 15:59:52 -0400

    Add createWeatherReport function
    This webpage will compare weather in Orlando to weather in
    Raleigh. Weather data is pulled from visualcrossing API.

--- src/script/index.js
+++ src/script/index.js
@@ -1,6 +1,9 @@
 import "../style/index.css";
 import { toggleTheme } from "../module/theme-toggle";
+import { createWeatherReport } from "../module/create-weather-report";
 
-let themeSlider = document.querySelector('#checkbox');
-themeSlider.addEventListener('click', toggleTheme);
+let themeSlider = document.querySelector("#checkbox");
+themeSlider.addEventListener("click", toggleTheme);
 toggleTheme();
+createWeatherReport('raleigh');
+createWeatherReport('orlando');
