cb8815d688270c2410f1c212b596017765d7eb9b
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-08-08 16:31:51 -0400

    Add weather report object
    Previously, the entirety of the weather report was logged.
    Now, only the desired information, stored as an object, is
    logged to console.

--- src/module/create-weather-report.js
+++ src/module/create-weather-report.js
@@ -17,8 +17,36 @@ async function createWeatherReport(city) {
     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=WNL4XSRW6RREQXLUGP6FJDDWE&contentType=json`,
     { mode: "cors" }
   );
-    const weatherData = await response.json();
-    console.log(weatherData);
+  const weatherData = await response.json();
+  const weatherReport = {
+    title: weatherData.resolvedAddress,
+    currentFeelsLike: weatherData.currentConditions.feelslike,
+    currentHumidity: weatherData.currentConditions.humidity,
+    currentTemperature: weatherData.currentConditions.temp,
+    currentConditionsIcon: weatherData.currentConditions.icon,
+    todayMaximumTemperature: weatherData.days[0].tempmax,
+    todayMinimumTemperature: weatherData.days[0].tempmin,
+    todayConditionsIcon: weatherData.days[0].icon,
+    dayTwoMaximumTemperature: weatherData.days[1].tempmax,
+    dayTwoMinimumTemperature: weatherData.days[1].tempmin,
+    dayTwoConditionsIcon: weatherData.days[1].icon,
+    dayThreeMaximumTemperature: weatherData.days[2].tempmax,
+    dayThreeMinimumTemperature: weatherData.days[2].tempmin,
+    dayThreeConditionsIcon: weatherData.days[2].icon,
+    dayFourMaximumTemperature: weatherData.days[3].tempmax,
+    dayFourMinimumTemperature: weatherData.days[3].tempmin,
+    dayFourConditionsIcon: weatherData.days[3].icon,
+    dayFiveMaximumTemperature: weatherData.days[4].tempmax,
+    dayFiveMinimumTemperature: weatherData.days[4].tempmin,
+    dayFiveConditionsIcon: weatherData.days[4].icon,
+    daySixMaximumTemperature: weatherData.days[5].tempmax,
+    daySixMinimumTemperature: weatherData.days[5].tempmin,
+    daySixConditionsIcon: weatherData.days[5].icon,
+    daySevenMaximumTemperature: weatherData.days[6].tempmax,
+    daySevenMinimumTemperature: weatherData.days[6].tempmin,
+    daySevenConditionsIcon: weatherData.days[6].icon,
   };
+  console.log(weatherReport);
+}
 
 export { createWeatherReport };
