73156c1371b1f1d383750f18dac21255a89e70cd
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-08-11 13:53:45 -0400

    Add dailyButton event listener
    When clicked the display is cleared and then populated
    with daily widgets. Daily button has its class set to
    'active' so that it gets highlighted and so the user knows
    they are currently seeing the daily view.

--- src/script/index.js
+++ src/script/index.js
@@ -9,7 +9,14 @@ import {
 import { toggleTheme } from "../module/theme-toggle";
 import { updateWeatherReport } from "../module/update-weather-report";
 import { generateDaily } from "../module/display-daily";
+import { clearDisplay } from "../module/clear-display";
+import { da } from "date-fns/locale";
 
 themeSlider.addEventListener("click", toggleTheme);
 toggleTheme();
 generateDaily("durham", "orlando");
+dailyButton.addEventListener("click", () => {
+  clearDisplay();
+  generateDaily("durham", "orlando");
+  dailyButton.setAttribute("class", "active");
+});
