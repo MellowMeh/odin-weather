e8cf924e727e77045cdcc6e41d3ce10691668971
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-08-11 15:07:52 -0400

    Add displayWeekly Function

--- src/script/index.js
+++ src/script/index.js
@@ -3,6 +3,7 @@ import { themeSlider, dailyButton, weeklyButton } from "../module/dom-query";
 import { toggleTheme } from "../module/theme-toggle";
 import { displayDaily } from "../module/display-daily";
 import { clearDisplay } from "../module/clear-display";
+import { displayWeekly } from "../module/display-weekly";
 
 themeSlider.addEventListener("click", toggleTheme);
 toggleTheme();
@@ -13,3 +14,9 @@ dailyButton.addEventListener("click", () => {
   dailyButton.setAttribute("class", "active");
   weeklyButton.setAttribute("class", "inactive");
 });
+weeklyButton.addEventListener('click', () => {
+  clearDisplay();
+  displayWeekly();
+  weeklyButton.setAttribute('class', 'active');
+  dailyButton.setAttribute('class', 'inactive');
+})
