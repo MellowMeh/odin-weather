db5461aa70c2539da1b7de734b82a818d0640110
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-08-11 13:50:57 -0400

    Add clearDisplay function

--- src/module/clear-display.js
+++ src/module/clear-display.js
@@ -1,3 +1,12 @@
+import { leftContainer, rightContainer } from "./dom-query";
+
 let clearDisplay = () => {
-  
-}
+  while (leftContainer.firstChild) {
+    leftContainer.removeChild(leftContainer.firstChild);
+  }
+  while (rightContainer.firstChild) {
+    rightContainer.removeChild(rightContainer.firstChild);
+  }
+};
+
+export { clearDisplay };
